1e25de122cc52c8a4c38e795e951c809f4ae87a9
조민기 <hodduco_o@naver.com>
2023-08-11 00:11:44 +0900

    00 - 스프링 시큐리티 커스터마이징: 인메모리 회원 추가 방식

--- src/main/java/just/springsecurity/config/SecurityConfig.java
+++ src/main/java/just/springsecurity/config/SecurityConfig.java
@@ -2,9 +2,15 @@ package just.springsecurity.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFilter;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -35,4 +41,13 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("jomg").password("{noop}123").roles("USER").build(),
+                User.withUsername("admin").password("{noop}123").roles("ADMIN").build()
+        );
+    }
+
 }
